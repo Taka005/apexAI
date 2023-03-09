c1594604a228a045bdfde5299db8f61f2d3a1438
Taka005 <01taka005@gmail.com>
2023-03-09 15:53:29 +0900

    追加

---
+++ main.py
@@ -0,0 +1,91 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import tensorflow as tf
+
+train_ds = tf.keras.utils.image_dataset_from_directory(
+  "dataset",
+  validation_split=0.2,
+  subset="training",
+  seed=123,
+  image_size=(735,735),
+  batch_size=32
+)
+
+val_ds = tf.keras.utils.image_dataset_from_directory(
+  "dataset",
+  validation_split=0.2,
+  subset="validation",
+  seed=123,
+  image_size=(735, 735),
+  batch_size=32
+)
+
+class_names = train_ds.class_names
+
+train_ds = train_ds.cache().shuffle(1000).prefetch(buffer_size=tf.data.AUTOTUNE)
+val_ds = val_ds.cache().prefetch(buffer_size=tf.data.AUTOTUNE)
+
+normalization_layer = tf.keras.layers.Rescaling(1./255)
+
+normalized_ds = train_ds.map(lambda x, y: (normalization_layer(x), y))
+image_batch, labels_batch = next(iter(normalized_ds))
+first_image = image_batch[0]
+
+model = tf.keras.Sequential([
+  tf.keras.layers.Rescaling(1./255, input_shape=(735, 735, 3)),
+  tf.keras.layers.Conv2D(16, 3, padding='same', activation='relu'),
+  tf.keras.layers.MaxPooling2D(),
+  tf.keras.layers.Conv2D(32, 3, padding='same', activation='relu'),
+  tf.keras.layers.MaxPooling2D(),
+  tf.keras.layers.Conv2D(64, 3, padding='same', activation='relu'),
+  tf.keras.layers.MaxPooling2D(),
+  tf.keras.layers.Flatten(),
+  tf.keras.layers.Dense(128, activation='relu'),
+  tf.keras.layers.Dense(len(class_names))
+])
+
+model.compile(
+    optimizer='adam',
+    loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+    metrics=['accuracy']
+)
+
+history = model.fit(
+  train_ds,
+  validation_data=val_ds,
+  epochs=10
+)
+
+acc = history.history['accuracy']
+val_acc = history.history['val_accuracy']
+
+loss = history.history['loss']
+val_loss = history.history['val_loss']
+
+plt.figure(figsize=(8, 8))
+plt.subplot(1, 2, 1)
+plt.plot(range(10), acc, label='Training Accuracy')
+plt.plot(range(10), val_acc, label='Validation Accuracy')
+plt.legend(loc='lower right')
+plt.title('Training and Validation Accuracy')
+
+plt.subplot(1, 2, 2)
+plt.plot(range(10), loss, label='Training Loss')
+plt.plot(range(10), val_loss, label='Validation Loss')
+plt.legend(loc='upper right')
+plt.title('Training and Validation Loss')
+plt.show()
+
+img = tf.keras.utils.load_img(
+   "test.png", target_size=(735,735)
+)
+img_array = tf.keras.utils.img_to_array(img)
+img_array = tf.expand_dims(img_array, 0)
+
+predictions = model.predict(img_array)
+score = tf.nn.softmax(predictions[0])
+
+print(
+    "この画像は {} に {:.2f}％類似しています"
+    .format(class_names[np.argmax(score)], 100 * np.max(score))
+)
